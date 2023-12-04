1b2dcf47aa79ec5746e96980fbae85fe79f3ce41
rifqi-anshari <46566132+rifqi-anshari@users.noreply.github.com>
2023-12-05 03:10:27 +0800

    Add files via upload

---
+++ js/modal.js
@@ -0,0 +1,39 @@
+// modal-01
+$("button[data-dismiss-modal=modal2]").click(function () {
+    $('#modal-01').modal('hide');
+});
+
+// modal-02
+$("button[data-dismiss-modal=modal2]").click(function () {
+    $('#modal-02').modal('hide');
+});
+
+// modal-03
+$("button[data-dismiss-modal=modal2]").click(function () {
+    $('#modal-03').modal('hide');
+});
+
+// modal-04
+$("button[data-dismiss-modal=modal2]").click(function () {
+    $('#modal-04').modal('hide');
+});
+
+// modal-lvl-01
+$("button[data-dismiss-modal=modal2]").click(function () {
+    $('#modal-lvl-01').modal('hide');
+});
+
+// modal-lvl-02
+$("button[data-dismiss-modal=modal2]").click(function () {
+    $('#modal-lvl-02').modal('hide');
+});
+
+// modal-lvl-03
+$("button[data-dismiss-modal=modal2]").click(function () {
+    $('#modal-lvl-03').modal('hide');
+});
+
+// modal-lvl-04
+$("button[data-dismiss-modal=modal2]").click(function () {
+    $('#modal-lvl-04').modal('hide');
+});
